11bb821dd2fc38c8b15a4d5eef50a417eb2035e0
Escona36 <eslamelhakem18@gmail.com>
2024-10-17 12:53:47 +0300

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب اســكــونــر لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
-await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
+    let message = '*مــنــور يـا قــلــب اســكــونــر لــي الـاوامــر اكــتــب (.اوامر) ❣️*';
+await conn.sendMessage(m.chat, { react: { text: '❣️', key: m.key } })
     conn.sendFile(m.chat, 'https://qu.ax/rovX.jpg', 'image.jpg', message, m);
 };
 
