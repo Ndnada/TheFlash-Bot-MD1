b1c2c0b713fcc218e10b528c50539509dc9df7ba
Escona36 <eslamelhakem18@gmail.com>
2024-10-21 13:33:41 +0300

    Update شغل.js

--- plugins/شغل.js
+++ plugins/شغل.js
@@ -1,159 +1,200 @@
 import fetch from 'node-fetch';
-import yts from 'yt-search';
-import ytdl from 'ytdl-core';
-import axios from 'axios';
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
-import { prepareWAMessageMedia, generateWAMessageFromContent } from '@whiskeysockets/baileys';
+import { prepareWAMessageMedia, generateWAMessageFromContent, getDevice } from '@whiskeysockets/baileys';
+import fs from 'fs';
 
-const handler = async (m, { command, usedPrefix, conn, args, text }) => {
-  if (command === 'شغل') {
-    if (!text) throw *❲ ❗ ❳ يرجي إدخال نص للبحث في يوتيوب .*\nمثال :\n> ➤  ${usedPrefix + command} القرآن الكريم\n> ➤  ${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p;
+let data;
+let buff;
+let mimeType;
+let fileName;
+let apiUrl;
+let apiUrl2;
+let apiUrlsz;
+let device;
+let dataMessage;
+let enviando = false;
 
-    try {
-      const yt_play = await search(args.join(' '));
-      const dataMessage = *❲ نتيجة البحث عن : ${text} ❳*\n➤ العنوان : ${yt_play[0].title}\n➤ النشر : ${yt_play[0].ago}\n➤ الطول : ${secondString(yt_play[0].duration.seconds)}\n➤ الرابط : ${yt_play[0].url}\n➤ المشاهدات : ${MilesNumber(yt_play[0].views)}\n➤ الصانع : ${yt_play[0].author.name}\n➤ القناة : ${yt_play[0].author.url}.trim();
+const handler = async (m, { command, usedPrefix, conn, text }) => {
+  device = await getDevice(m.key.id);
 
-      const iturl = yt_play[0].url;
-      const itimg = yt_play[0].thumbnail;
-      const messa = await prepareWAMessageMedia({ image: { url: itimg } }, { upload: conn.waUploadToServer });
+  if (!text) throw `*[ ℹ️ ] ينقص عنوان فيديو YouTube.*\n\n*[ 💡 ] مثال:* _${usedPrefix + command} Good Feeling - Flo Rida_\n\n*[ 💡 ] مثال 2:* _${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p_`;
 
+  if (command === 'playyt' && (device == 'desktop' || device == 'web')) throw `*[❗] رسائل الأزرار غير متوفرة في WhatsApp web، يرجى استخدام هاتفك المحمول لعرض الرسائل واستخدام الأزرار.*`;
+
+  if (enviando) return;
+  enviando = true;
+
+  try {
+    apiUrlsz = [
+      `https://api.cafirexos.com/api/ytplay?text=${text}`,
+      `https://api-brunosobrino.onrender.com/api/ytplay?text=${text}&apikey=BrunoSobrino`,
+      `https://api-brunosobrino-dcaf9040.koyeb.app/api/ytplay?text=${text}`
+    ];
+    const linkyt = await isValidYouTubeLink(text);
+    if (linkyt) {
+      apiUrlsz = [
+        `https://api.cafirexos.com/api/ytinfo?url=${text}`,
+        `https://api-brunosobrino-koiy.onrender.com/api/ytinfo?url=${text}&apikey=BrunoSobrino`,
+        `https://api-brunosobrino-dcaf9040.koyeb.app/api/ytinfo?url=${text}`
+      ];
+    }
+
+    let success = false;
+    for (const url of apiUrlsz) {
+      try {
+        const res = await fetch(url);
+        data = await res.json();
+        if (data.resultado && data.resultado.url) {
+          success = true;
+          break;
+        }
+      } catch {}
+    }
+
+    if (!success) {
+      enviando = false;
+      throw `_*< يوتيوب - تحميل />*_\n\n*[ ℹ️ ] ينقص عنوان فيديو YouTube.*\n\n*[ 💡 ] مثال:* _.شغل Good Feeling - Flo Rida_\n\n*[ 💡 ] مثال 2:* _.شغل https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p_`;
+    }
+
+    const dataMessage = `العنوان : ${data.resultado.title}\nتم النشر : ${data.resultado.publicDate}\nالقناه : ${data.resultado.channel}\nرابط القناه : ${data.resultado.url}`.trim();
+
+    if (!text.includes('SN@') && command !== 'شغل') {
+      await conn.sendMessage(m.chat, { text: dataMessage }, { quoted: m });
+    }
+
+    if (command === 'شغل') {
+      const messa = await prepareWAMessageMedia({ image: { url: data.resultado.image } }, { upload: conn.waUploadToServer });
       let msg = generateWAMessageFromContent(m.chat, {
         viewOnceMessage: {
           message: {
             interactiveMessage: {
               body: { text: dataMessage },
-              footer: { text: ©${global.wm}.trim() },
+              footer: { text: `𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿🦦`.trim() },
               header: {
                 hasMediaAttachment: true,
                 imageMessage: messa.imageMessage,
               },
               nativeFlowMessage: {
                 buttons: [
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'صوتي', id: ${usedPrefix}mp3.1 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'ملف صوتي', id: ${usedPrefix}mp3.2 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'ريكورد', id: ${usedPrefix}mp3.3 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'فيديو', id: ${usedPrefix}mp4.1 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'ملف فيديو', id: ${usedPrefix}mp4.2 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'جيف', id: ${usedPrefix}mp4.3 ${iturl} }) },
+                  {
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                      display_text: 'الصوت🎧',
+                      id: `${usedPrefix}play.1 ${data.resultado.url} SN@`
+                    })
+                  },
+                  {
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                      display_text: 'الفيديو📽️',
+                      id: `${usedPrefix}play.2 ${data.resultado.url} SN@`
+                    })
+                  },
                 ],
                 messageParamsJson: "",
               },
             },
           },
-        },
+        }
       }, { userJid: conn.user.jid, quoted: m });
-
       await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
-
-    } catch {
-      throw *❲ ❗ ❳ حدث خطأ عند البحث في يوتيوب .*\nيرجي ادخال نص صحيح أو رابط مثال :\n> ➤  ${usedPrefix + command} القرآن الكريم\n> ➤  ${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p;
+      enviando = false;
+      return;
     }
 
-  } else if (command.startsWith('mp3.')) {
     try {
-      const q = '128kbps';
-      const v = text;
-      const yt = await youtubedl(v).catch(async () => await youtubedlv2(v));
-      const dl_url = await yt.audio[q].download();
-      const ttl = await yt.title;
+      if (command === 'play.1') {
+        let apiUrls2 = [
+          `https://api.cafirexos.com/api/v1/ytmp3?url=${data.resultado.url}`,
+          `https://api.cafirexos.com/api/v2/ytmp3?url=${data.resultado.url}`,
+          `https://api-brunosobrino.onrender.com/api/v1/ytmp3?url=${data.resultado.url}&apikey=BrunoSobrino`,
+          `https://api-brunosobrino.onrender.com/api/v2/ytmp3?url=${data.resultado.url}&apikey=BrunoSobrino`,
+          `https://api-brunosobrino-dcaf9040.koyeb.app/api/v1/ytmp3?url=${data.resultado.url}`,
+          `https://api-brunosobrino-dcaf9040.koyeb.app/api/v2/ytmp3?url=${data.resultado.url}`
+        ];
 
-      if (command === 'mp3.1') {
-        await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: 'audio/mpeg', fileName: ${ttl}.mp3 }, { quoted: m });
-      } else if (command === 'mp3.2') {
-        await conn.sendMessage(m.chat, { document: { url: dl_url }, mimetype: 'audio/mpeg', fileName: ${ttl}.mp3 }, { quoted: m });
-      } else if (command === 'mp3.3') {
-        await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: 'audio/ogg; codecs=opus', ptt: true, fileName: ${ttl}.opus }, { quoted: m });
+        let success2 = false;
+        for (const urll of apiUrls2) {
+          try {
+            apiUrl2 = urll;
+            mimeType = 'audio/mpeg';
+            fileName = 'audio.mp3';
+            buff = await conn.getFile(apiUrl2);
+            success2 = true;
+            break;
+          } catch {}
+        }
+
+        if (!success2) {
+          enviando = false;
+          throw `حدث خطأ أثناء جلب رابط الصوت.`;
+        }
+      } else if (command === 'play.2') {
+        let apiUrls22 = [
+          `https://api.cafirexos.com/api/v1/ytmp4?url=${data.resultado.url}`,
+          `https://api.cafirexos.com/api/v2/ytmp4?url=${data.resultado.url}`,
+          `https://api-brunosobrino.onrender.com/api/v1/ytmp4?url=${data.resultado.url}&apikey=BrunoSobrino`,
+          `https://api-brunosobrino.onrender.com/api/v2/ytmp4?url=${data.resultado.url}&apikey=BrunoSobrino`,
+          `https://api-brunosobrino-dcaf9040.koyeb.app/api/v1/ytmp4?url=${data.resultado.url}`,
+          `https://api-brunosobrino-dcaf9040.koyeb.app/api/v2/ytmp4?url=${data.resultado.url}`
+        ];
+
+        let success2 = false;
+        for (const urlll of apiUrls22) {
+          try {
+            apiUrl2 = urlll;
+            mimeType = 'video/mp4';
+            fileName = 'video.mp4';
+            buff = await conn.getFile(apiUrl2);
+            success2 = true;
+            break;
+          } catch (e) {
+            console.log(e.message);
+          }
+        }
+
+        if (!success2) {
+          enviando = false;
+          throw `حدث خطأ أثناء جلب رابط الفيديو.`;
+        }
       }
-
-    } catch (error) {
-      await handleFallback(command, text, m, conn);
+    } catch (ee) {
+      console.log(ee.message);
+      enviando = false;
+      throw `حدث خطأ غير متوقع.`;
     }
 
-  } else if (command.startsWith('mp4.')) {
-    try {
-      const qu = '360';
-      const q = ${qu}p;
-      const v = text;
-      const yt = await youtubedl(v).catch(async () => await youtubedlv2(v));
-      const dl_url = await yt.video[q].download();
-      const ttl = await yt.title;
-      const size = await yt.video[q].fileSizeH;
-
-      if (command === 'mp4.1') {
-        await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: ➤ العنوان : ${ttl}\n➤ الحجم : ${size} }, { quoted: m });
-      } else if (command === 'mp4.2') {
-        await conn.sendMessage(m.chat, { document: { url: dl_url }, fileName: ${ttl}.mp4, mimetype: 'video/mp4', caption: ➤ العنوان : ${ttl}\n➤ الحجم : ${size}, thumbnail: await fetch(yt.thumbnail) }, { quoted: m });
-      } else if (command === 'mp4.3') {
-        await conn.sendMessage(m.chat, { video: { url: dl_url }, gifPlayback: true, caption: ➤ العنوان : ${ttl}\n➤ الحجم : ${size} }, { quoted: m });
-      }
-
-    } catch (error) {
-      await handleFallback(command, text, m, conn);
+    if (buff) {
+      await conn.sendMessage(m.chat, { [mimeType.startsWith('audio') ? 'audio' : 'video']: buff.data, mimetype: mimeType, fileName: fileName }, { quoted: m });
+      enviando = false;
+    } else {
+      enviando = false;
+      throw `حدث خطأ أثناء إرسال الملف.`;
     }
+  } catch (error) {
+    console.log(error);
+    enviando = false;
+    throw `حدث خطأ أثناء المعالجة.`;
   }
 };
 
-handler.command = /^(شغول|mp3.1|mp3.2|mp3.3|mp4.1|mp4.2|mp4.3)$/i;
+handler.command = /^(play.1|play.2|شغل)$/i;
 export default handler;
 
-async function search(query, options = {}) {
-  const search = await yts.search({ query, hl: 'ar', gl: 'AR', ...options });
-  return search.videos;
-}
-
-function MilesNumber(number) {
-  const exp = /(\d)(?=(\d{3})+(?!\d))/g;
-  const rep = '$1.';
-  const arr = number.toString().split('.');
-  arr[0] = arr[0].replace(exp, rep);
-  return arr[1] ? arr.join('.') : arr[0];
-}
-
-function secondString(seconds) {
-  seconds = Number(seconds);
-  const d = Math.floor(seconds / (3600 * 24));
-  const h = Math.floor((seconds % (3600 * 24)) / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = Math.floor(seconds % 60);
-  const dDisplay = d > 0 ? d + (d == 1 ? ' día, ' : ' días, ') : '';
-  const hDisplay = h > 0 ? h + (h == 1 ? ' hora, ' : ' horas, ') : '';
-  const mDisplay = m > 0 ? m + (m == 1 ? ' minuto, ' : ' minutos, ') : '';
-  const sDisplay = s > 0 ? s + (s == 1 ? ' segundo' : ' segundos') : '';
-  return dDisplay + hDisplay + mDisplay + sDisplay;
-}
-
-function bytesToSize(bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return 'n/a';
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-  return i === 0 ? ${bytes} ${sizes[i]} : ${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]};
-}
-
-async function ytMp3(url) {
-  return new Promise((resolve, reject) => {
-    ytdl.getInfo(url).then(async (getUrl) => {
-      const result = [];
-      for (let i = 0; i < getUrl.formats.length; i++) {
-        const item = getUrl.formats[i];
-        if (item.mimeType === 'audio/webm; codecs="opus"') {
-          const { contentLength } = item;
-          const bytes = await bytesToSize(contentLength);
-          result[i] = { audio: item.url, size: bytes };
-        }
+async function isValidYouTubeLink(link) {
+  const validPatterns = [
+    /youtube\.com\/watch\?v=/i,
+    /youtube\.com\/shorts\//i,
+    /youtu\.be\//i,
+    /youtube\.com\/embed\//i,
+    /youtube\.com\/v\//i,
+    /youtube\.com\/attribution_link\?a=/i,
+    /yt\.be\//i,
+    /googlevideo\.com\//i,
+    /youtube\.com\.br\//i,
+    /youtube-nocookie\.com\//i,
+    /youtubeeducation\.com\//i,
+    /m\.youtube\.com\//i,
+    /youtubei\.googleapis\.com\//i
+  ];
+  return validPatterns.some(pattern => pattern.test(link));
       }
-      const fix = result.filter((x) => x.audio !== undefined && x.size !== undefined);
-      resolve(fix[0]);
-    }).catch(reject);
-  });
-}
-
-async function handleFallback(command, text, m, conn) {
-  const isMp3 = command.startsWith('mp3.');
-  const result = await ytMp3(text);
-
-  if (isMp3) {
-    await conn.sendMessage(m.chat, { audio: { url: result.audio }, fileName: audio.mp3, mimetype: 'audio/mpeg', ptt: false, fileLength: result.size }, { quoted: m });
-  } else {
-    await conn.sendMessage(m.chat, { video: { url: result.audio }, fileName: video.mp4, mimetype: 'video/mp4', fileLength: result.size }, { quoted: m });
-  }
-}
