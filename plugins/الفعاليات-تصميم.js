82acc46420574897f073b8c30ffd772b67a3f203
Escona36 <eslamelhakem18@gmail.com>
2024-10-23 15:35:11 +0300

    Update الفعاليات-تصميم.js

--- plugins/الفعاليات-تصميم.js
+++ plugins/الفعاليات-تصميم.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
   if (!args[0]) throw '*\`『 نسيت النص يحب ​​​​​​​​​​​​​​​​​​​🗿 』\`* '
   m.reply('*\`『 ويت بتحمل ❤️‍🔥🙈 』\`*')
   let res = `https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&script=fluffy-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=${response[0]}`
-  conn.sendFile(m.chat, res, 'Noureddine.jpg', ` *\`『 𝐸𝐿-𝐷𝐸𝑺𝑂𝐾𝐸𝐼𝑌 𝐵𝑂𝑇 』\`* `, m, false)
+  conn.sendFile(m.chat, res, 'Noureddine.jpg', ` *\`『 اسكونر بيمسي عليك​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​😙❣️』\`* `, m, false)
 }
 handler.help = ['flaming1'].map(v => v + ' <text>')
 handler.tags = ['maker','logo']
