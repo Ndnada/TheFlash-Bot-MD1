1e601ced48dbf0e106d26f52dd9b44f99039e563
Escona36 <eslamelhakem18@gmail.com>
2024-10-23 14:34:16 +0300

    Update انشر-خاص.js

--- plugins/انشر-خاص.js
+++ plugins/انشر-خاص.js
@@ -1,17 +1,22 @@
-import fs from 'fs';
-const handler = async (m, {conn, text} ) => {
-const chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map((v) => v[0]);
-if (!text) throw '*⚠️  إدخل الرسالة اللي عايز تبعتها*'
-const cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m;
-const teks = text ? text : cc.text;
-for (const i of chats) {
-await delay(500);
-conn.sendMessage(i, { text: `✅ *إعلان رسمي* ✅\n\n` + teks, mentions: [m.sender], mentions: (await conn.groupMetadata(id)).participants.map(v => v.id) }, { quoted: fkontak })}
-m.reply(`*[❗] الرسالة اتبعتت لـ ${chats.length} شات خاص*\n\n*ملحوظة: ممكن يكون فيه مشاكل في الأمر ده وماتبعتش لكل الشاتس، آسفين لحد دلوقتي*`)};
-handler.help = ['broadcastchats', 'bcchats'].map((v) => v + ' <teks>');
-handler.tags = ['owner'];
-handler.command = /^(broadcastchats?|انشر_خاص(hats?)?)$/i;
-handler.rowner = true;
-export default handler;
+//import Connection from '../lib/connection.js'
+import { randomBytes } from 'crypto'
 
-const delay = (time) => new Promise((res) => setTimeout(res, time));
+let handler = async (m, { conn, text }) => {
+  let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
+  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `جاري التجهيز *عدد الجروبات:* ${chats.length}`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast|tx/i.test(teks) ? teks : `${teks}` ), true).catch(_ => _)
+  m.reply('تم التحويل')
+}
+handler.help = ['tx']
+handler.tags = ['owner']
+handler.command = /^(نشر)$/i
+handler.owner = true
+
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
