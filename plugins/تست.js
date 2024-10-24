f3e2997ea4e227e5927f0e23d52854238a392585
Escona36 <eslamelhakem18@gmail.com>
2024-10-24 10:52:39 +0300

    Update تست.js

--- plugins/تست.js
+++ plugins/تست.js
@@ -1,13 +1,19 @@
-import fs from 'fs';
-const handler = (m) => m;
-handler.all = async function(m) {
-  const sounds4 = "./media/الصدمه.mp3";
-  const chat = global.db.data.chats[m.chat];
-  if (/^تست$/i.test(m.text) && !chat.isBanned) {
-    conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*شغال يزمكس😺*`);
-    m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
-  }
-  return !0;
-};
-export default handler;
+//*رد بوت اسكونر 
+
+let handler = m => m; 
+ handler.all = async function (m) { 
+
+   let chat = global.db.data.chats[m.chat]; 
+   let responses; 
+   if (/^تست$/i.test(m.text)) { 
+     responses = ['شغال وعالي العال😘*']; 
+ }
+  if (/^تست$/i.test(m.text)) {
+    let vn =     'https://qu.ax/cZJu'    ;
+    conn.sendPresenceUpdate(    'recording'    , m.chat);
+    conn.sendMessage(m.chat, { audio: { url: vn }, ptt: true, mimetype:     'audio/mpeg'    , fileName: `shawaza_zizo_2024.opp` }, { quoted: m });
+  }   
+   return !0 
+ }; 
+
+ export default handler;
